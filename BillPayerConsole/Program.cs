e292b625910b7f154214344d566b0838d4d9bdbb
Judah Perez <tythin4@gmail.com>
2016-02-18 10:16:07 -0800

    Added period to hello world

--- BillPayerConsole/Program.cs
+++ BillPayerConsole/Program.cs
@@ -7,7 +7,7 @@ namespace BillPayerConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world");
+            Console.WriteLine("Hello, world.");
 
             OtherClass other = new OtherClass();
             TestClass test = new TestClass();
