824554989f05b86e9e94061e1be60fffd31757d7
stuckonmusic <prestonrichey@gmail.com>
2016-04-26 10:27:19 -0700

    Roommate Search
    Cleaned up home page, and created roommate search

--- BillPayingWebsite/Controllers/HouseController.cs
+++ BillPayingWebsite/Controllers/HouseController.cs
@@ -33,6 +33,7 @@ namespace BillPayingWebsite.Controllers
             }
         }
 
+        [Authorize]
         public async Task<ActionResult> Index()
         {
             
---
+++ BillPayingWebsite/Controllers/RoommatesController.cs
@@ -0,0 +1,46 @@
+﻿using BillPayerCore.Data;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BillPayingWebsite.Controllers
+{
+    public class RoommatesController : Controller
+    {
+
+
+        DataContext dbContext = new DataContext();
+
+        private ApplicationUserManager _userManager;
+
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+
+
+        // GET: Roommates
+
+        [Authorize]
+        public async Task<ActionResult> Index()
+        {
+
+            // var user = await UserManager.FindByIdAsync(User.Identity.GetUserId());
+            var roommates = dbContext.Users.ToList();
+            return View(roommates);
+        }
+
+    }
+}
